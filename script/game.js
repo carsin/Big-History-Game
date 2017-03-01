93b8d747bfca668a9060e0014cc837f5a6604364
Carson <carsonfreedman1@gmail.com>
2017-02-28 17:27:58 -1000

    Finish Group Create Page

---
+++ script/game.js
@@ -0,0 +1,13 @@
+function createGroups() {
+	var groupName1 = document.getElementById("group-name1").value;
+	var groupPopulation1 = document.getElementById("population-1").value;
+
+	var groupName2 = document.getElementById("group-name2").value;
+	var groupPopulation2 = document.getElementById("population-2").value;
+
+	var groupName3 = document.getElementById("group-name3").value;
+	var groupPopulation3 = document.getElementById("population-3").value;
+	
+	var groupName4 = document.getElementById("group-name4").value;
+	var groupPopulation4 = document.getElementById("population-4").value;
+}
